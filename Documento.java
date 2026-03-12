919777251b7da126bd0e6b306d769d0859cbd480
Nicolas Alarcon <hevona1298@keecs.com>
2026-03-12 10:46:48 -0500

    Metodos principales y atributos de clases

--- Documento.java
+++ Documento.java
@@ -1,3 +1,17 @@
 public class Documento {
-    
+
+    Documento() { //Constructor por defecto
+    }
+
+    public void insertarLinea(int posicion, String texto) {
+
+    }
+
+    public void borrarLinea(int posicion) {
+
+    }
+
+    public void mostrarDocumento() {
+        
+    }
 }
--- EditorTexto.java
+++ EditorTexto.java
@@ -1,3 +1,6 @@
 public class EditorTexto {
+    private Documento documentoActual;
+    //Aquí van a ir las pilas
+
 
 }
