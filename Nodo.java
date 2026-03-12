3eeceaad70f4501bdb53e50c6859d30ddd8a6cd9
Nicolas Alarcon <hevona1298@keecs.com>
2026-03-12 09:35:32 -0500

    Eliminar metodos y actualizar readme

--- Nodo.java
+++
@@ -1,9 +0,0 @@
-public class Nodo<T> { //Comentario
-    T dato;
-    Nodo siguiente;
-
-    public Nodo(T dato) {
-        this.dato = dato;
-        this.siguiente = null;
-    }
-}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ﻿# Proyecto Programación III - Nicolás Alarcón
 
+## Tema del proyecto: Simulador de editor de texto
