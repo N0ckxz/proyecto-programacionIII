0005e8fe0a90c50024b48e4766b96e2400bff676
Nicolas Alarcon <hevona1298@keecs.com>
2026-03-12 09:33:46 -0500

    Quitar clases de lista e ingresar metodos fundamentales

--- Main.java
+++ Main.java
@@ -1,8 +1,3 @@
 public class Main { 
-    public static void main(String[] args) {
-        ColaEnlazada<String> cola = new ColaEnlazada<String>();
-        System.out.println(cola);
-        cola.encolar("¡Saludos!");
-        System.err.println(cola);
-    }
+    
 }
