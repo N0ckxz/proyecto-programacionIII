071e934c52efcd62cf61a7662821ed8be51e1812
Nicolas Alarcon <est.nicolas.alarcon@unimilitar.edu.co>
2026-03-19 10:41:19 -0500

    Agregar librerias del profesor

---
+++ proyecto/Documento.java
@@ -0,0 +1,17 @@
+public class Documento { //Comentario de prueba en casa
+
+    Documento() { //Constructor por defecto
+    }
+
+    public void insertarLinea(int posicion, String texto) {
+
+    }
+
+    public void borrarLinea(int posicion) {
+
+    }
+
+    public void mostrarDocumento() {
+        
+    }
+}
---
+++ proyecto/EditorTexto.java
@@ -0,0 +1,6 @@
+public class EditorTexto {
+    private Documento documentoActual;
+    //Aquí van a ir las pilas
+
+
+}
